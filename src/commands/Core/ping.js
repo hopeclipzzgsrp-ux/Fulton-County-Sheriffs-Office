9f20b296585e28d05114ca1b743ef483f4233c7b
hopeclipzzgsrp-ux <hopeclipzz.gsrp@gmail.com>
2026-06-13 17:33:35 -0500

    Update ping.js

--- src/commands/Core/ping.js
+++ src/commands/Core/ping.js
@@ -53,3 +53,15 @@ export default {
 
 
 
+import { SlashCommandBuilder } from 'discord.js';
+
+export const command = {
+    data: new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('Replies with Pong and latency!'),
+    async execute(interaction) {
+        const sent = await interaction.reply({ content: 'Pinging...', fetchReply: true });
+        const latency = sent.createdTimestamp - interaction.createdTimestamp;
+        await interaction.editReply(`Pong! 🏓\nBot Latency: ${latency}ms`);
+    },
+};
